fedd8ea052ee55787249b6208a6d73ad57b023a5
cil44 <cyliaa1411@gmail.com>
2026-03-18 10:10:31 +0900

    feat(config): Update invite URL with permissions
    Includes necessary permissions and scopes in the bot's invite URL to ensure full functionality and command access.

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const config = {
   botName: "Yumeko",
   botAvatar: "https://i.imgur.com/7x2G2tN.gif",
   botBanner: "https://i.imgur.com/7x2G2tN.gif",
-  inviteURL: "https://discord.com/oauth2/authorize?client_id=1481062537339473920",
+  inviteURL: "https://discord.com/oauth2/authorize?client_id=1481062537339473920&permissions=8&integration_type=0&scope=bot+applications.commands",
   supportServer: "https://discord.gg/a46m6Ywh5n",
   tagline: "The Best Global Economy & Casino Discord Bot",
   footerText: "© 2026 Yumeko Bot\nAll rights reserved\nyumekobot.online",
